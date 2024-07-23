b7608c4dc3429c205b8267ddf7470e131453cbaa
Reza Khoddammohammadi <rkhoddammohammadi@my.bcit.ca>
2024-07-23 14:27:08 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function changeText() {
+    document.getElementById('text').innerText = 'The text has been changed!';
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = '#ffcccb';
+}
+
+function revealOffer() {
+    document.getElementById('discountCode').classList.toggle('hidden');
+}
+
+// Change text on button click
+function addToCart(button) {
+    button.innerText = 'Added!';
+}
+
+// Example to change profile picture
+function changeProfilePicture() {
+    document.querySelector('.profile-picture').src = 'images/new-profile.png';
+}
+
+function changeProfilePicture() {
+    document.querySelector('.profile-picture').src = 'images/new-profile.png';
+}
